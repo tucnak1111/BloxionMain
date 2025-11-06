c30c0cb4769ddad58e8db75e35c38800a242e8b8
Bloxion <angee.3@icloud.com>
2025-11-06 15:33:54 +0100

    Create logout.ts

---
+++ api/auth/logout.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader(
+    "Set-Cookie",
+    `bloxion_auth=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0`
+  );
+  res.status(200).json({ success: true });
+}
