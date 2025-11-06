add26417503e039cc39bb1b9b5449b83a4c5ebf5
Bloxion <angee.3@icloud.com>
2025-11-06 15:26:15 +0100

    Update page.tsx — typo

--- app/workspace/[id]/users/page.tsx
+++ app/workspace/[id]/users/page.tsx
@@ -1,4 +1,4 @@
-import UserList from "../components/UserList";
+import UserList from "../components/UserList.tsx";
 
 export default function WorkspaceUsersPage({ params }: { params: { id: string } }) {
   return (
