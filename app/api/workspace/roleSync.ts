a504e03f7a60a2a27b69f875c7524003d78a8841
Bloxion <angee.3@icloud.com>
2025-11-16 17:21:25 +0100

    Update roleSync.ts

--- app/api/workspace/roleSync.ts
+++ app/api/workspace/roleSync.ts
@@ -43,7 +43,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
       
       const existing = await prisma.workspaceMember.findFirst({
-        where: { workspaceId: workspace.id, robloxId: robloxId.toString() },
+        where: { workspaceId: workspace.id, userId: userId.toString() },
       });
 
       if (hasAccess) {
