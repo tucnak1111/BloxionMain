47d0d62f2bf21bcb48acfe747473b2b934d2e9b0
Bloxion <angee.3@icloud.com>
2025-11-16 19:01:49 +0100

    Update route.ts

--- app/api/internal/ban/route.ts
+++ app/api/internal/ban/route.ts
@@ -1,12 +1,13 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../prisma/Client";
-
+if (req.method === "GET") {
+  return res.status(200).json({ message: "status OK" })
+}
 export async function POST(req: Request) {
   const auth = req.headers.get("x-api-key");
   if (auth !== process.env.INTERNAL_BAN_API_KEY) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
-
   const body = await req.json();
   const { robloxId, reason, bannedBy } = body;
 
