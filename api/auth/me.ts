6ba24f199d2ab7be0603818d2d74c6b51319afea
Bloxion <angee.3@icloud.com>
2025-11-16 10:35:07 +0100

    Update me.ts

--- api/auth/me.ts
+++ api/auth/me.ts
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { prisma } from "@/lib/prisma";
+import { prisma } from "../../prisma/Client";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
