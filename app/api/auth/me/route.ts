97fd2c392907f2c0b389cf1c835cde5ef34e91eb
Bloxion <angee.3@icloud.com>
2025-11-16 21:01:55 +0100

    Update route.ts

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
-import { prisma } from "@/prisma/Client"; // adjust if needed
+import { prisma } from "../../../../prisma/Client"; // adjust if needed
 
 export async function GET(req: Request) {
   const cookie = req.headers.get("cookie");
