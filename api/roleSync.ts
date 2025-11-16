3da00c1d3010b6bf9e130ce8ee9308e6d54b91a2
Bloxion <angee.3@icloud.com>
2025-11-16 10:27:30 +0100

    Update roleSync.ts

--- api/roleSync.ts
+++ api/roleSync.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
-import { prisma } from "../prisma/client";
+import { prisma } from "../prisma";
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
