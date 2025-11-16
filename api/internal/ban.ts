d677ab6980d7211c2f2d43db1e05724cfaf05430
Bloxion <angee.3@icloud.com>
2025-11-16 10:40:30 +0100

    Update ban.ts

--- api/internal/ban.ts
+++ api/internal/ban.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import { prisma } from "../../prisma/Client";
 
 export async function POST(req: Request) {
   const auth = req.headers.get("x-api-key");
