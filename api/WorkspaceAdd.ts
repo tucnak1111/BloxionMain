fd86d6bd2b2d01a0d3fa35c0f724a14abc179540
Bloxion <angee.3@icloud.com>
2025-11-16 10:26:56 +0100

    Update WorkspaceAdd.ts

--- api/WorkspaceAdd.ts
+++ api/WorkspaceAdd.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "../prisma/client"; 
+import { prisma } from "../prisma"; 
 import { z } from "zod";
 
 const WorkspaceSchema = z.object({
