ea62423886259e1a3a3f43795fdbf2575cd47b49
Bloxion <angee.3@icloud.com>
2025-11-17 23:47:51 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,11 @@
-export const metadata = {
-  title: "Bloxion"
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
