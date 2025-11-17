5176a85aaa4505b2be2f0db89f56f13665cd52ce
Bloxion <angee.3@icloud.com>
2025-11-17 23:44:48 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,3 @@
-import { cookies } from "next/headers";
-import jwt from "jsonwebtoken";
-import { redirect } from "next/navigation";
-
-export default function AppLayout({ children }) {
-  const token = cookies().get("bloxion_auth")?.value;
-  if (!token) redirect("/login");
-
-  try {
-    jwt.verify(token, process.env.JWT_SECRET!);
-  } catch {
-    redirect("/login");
-  }
-
-  return <>{children}</>;
+export const metadata {
+  title: "Bloxion"
 }
