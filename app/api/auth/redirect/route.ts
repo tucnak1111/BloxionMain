baf71bc5a6dcd80a6f89309c32cd46bbb8240617
Bloxion <angee.3@icloud.com>
2025-11-30 11:50:40 +0100

    CRITICAL: Validate state

--- app/api/auth/redirect/route.ts
+++ app/api/auth/redirect/route.ts
@@ -1,10 +1,16 @@
 import { NextResponse } from "next/server";
-
+import { cookies } from "next/headers";
 export function GET() {
   const ROBLOX_CLIENT_ID = process.env.ROBLOX_CLIENT_ID!;
   const REDIRECT_URI = process.env.ROBLOX_REDIRECT_URI!;
   const STATE = crypto.randomUUID();
 
+  cookies().set("roblox_oauth_state", STATE, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    maxAge: 300,
+  });
   const authUrl = `https://apis.roblox.com/oauth/v1/authorize?client_id=${ROBLOX_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(
     REDIRECT_URI
   )}&scope=openid%20profile&state=${STATE}`;
