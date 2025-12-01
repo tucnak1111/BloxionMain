c80cec33bbd1f2d2513aa08016b6182334a161c6
bloxion <angee.3@icloud.com>
2025-12-01 13:34:01 +0100

    fix - layoutWrapper.tsx

--- app/LayoutWrapper.tsx
+++ app/LayoutWrapper.tsx
@@ -33,7 +33,7 @@ export default function LayoutWrapper({
     <>
       <Navbar toggleSidebar={toggleSidebar} user={user} openSettings={openSettings} />
       <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} user={user} openSettings={openSettings} />
-      <main>{children}</main>
+      <main style={{ paddingTop: "60px" }}>{children}</main>
       <Settings isOpen={isSettingsOpen} onClose={() => setSettingsOpen(false)} />
     </>
   );
