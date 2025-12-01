9b28357f2ecad8b907f04934cc6b128cc1b7db3f
bloxion <angee.3@icloud.com>
2025-12-01 14:18:14 +0100

    text on loading screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,10 @@ import "./loading.css";
 
 export default function Loading() {
   return (
+    
+    
     <div className="loading-overlay">
+      Bloxion
       <div className="spinner"></div>
     </div>
   );
