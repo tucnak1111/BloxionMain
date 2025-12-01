9644cb125728aea90fd673be834ec7c37c292980
bloxion <angee.3@icloud.com>
2025-12-01 14:23:49 +0100

    fix

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,8 +2,6 @@ import "./loading.css";
 
 export default function Loading() {
   return (
-    
-    
     <div className="loading-overlay">
       <div className="title"> Bloxion </div>
       <div className="spinner"></div>
