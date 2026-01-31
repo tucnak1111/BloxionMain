c43d7d9e58a849aa9d548449b95fddf9f60cba12
bloxion <angee.3@icloud.com>
2026-01-31 16:12:16 +0100

    support for electron

---
+++ electron/main.js
@@ -0,0 +1,23 @@
+const {app, BrowserWindow} = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true, nodeIntegration: false,
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:3000");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
+});
